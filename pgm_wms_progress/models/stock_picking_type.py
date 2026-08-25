7c2a54da2d7e3c391e3cd07f5ef409c76cefa5db
jmarmolejo_summa <jmarmolejo_summa@outlook.com>
2026-08-25 08:36:08 -0500

    Fix UTF-8 encoding in stock_picking_type.py

--- pgm_wms_progress/models/stock_picking_type.py
+++ pgm_wms_progress/models/stock_picking_type.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ï»¿# -*- coding: utf-8 -*-
 
 from odoo import api, models, fields
 
@@ -7,21 +7,21 @@ class StockPickingType(models.Model):
 
     seguimiento_wms = fields.Boolean(
         string='Seguimiento WMS',
-        help="Si está marcado, habilita el rastreo de progreso, velocidad y tiempos en la PDA para este tipo de operación."
+        help="Si esta marcado, habilita el rastreo de progreso, velocidad y tiempos en la PDA para este tipo de operacion."
     )
     is_aduana_wms = fields.Boolean(
         string='Es Aduana (WMS)',
-        help="Identifica de forma segura que esta operación es una Aduana."
+        help="Identifica de forma segura que esta operacion es una Aduana."
     )
     is_picking_wms = fields.Boolean(
         string='Es Picking (WMS)',
-        help="Identifica de forma segura que esta operación es un Picking."
+        help="Identifica de forma segura que esta operacion es un Picking."
     )
     is_entrega_wms = fields.Boolean(
         string='Es Entrega (WMS)',
-        help="Identifica de forma segura que esta operación es una Entrega Final."
+        help="Identifica de forma segura que esta operacion es una Entrega Final."
     )
     is_recibo_pt_wms = fields.Boolean(
         string='Es Recibo PT (WMS)',
-        help="Identifica de forma segura que esta operación es un Recibo de Producto Terminado."
+        help="Identifica de forma segura que esta operacion es un Recibo de Producto Terminado."
     )
